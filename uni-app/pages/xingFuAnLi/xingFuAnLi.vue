c52967b9b7cc475106b2aa2871c18be5578f10a5
sunliguo <sunliguo2006@qq.com>
2024-03-06 23:23:54 +0800

    幸福案例前端展示

--- uni-app/pages/xingFuAnLi/xingFuAnLi.vue
+++ uni-app/pages/xingFuAnLi/xingFuAnLi.vue
@@ -1,10 +1,17 @@
 <template>
 	<view>
-		<view>我们脱单了</view>
-		<view v-for="item in anli">
-			<view>{{item._id}}</view>
+		<view v-for="item in anli.result">
+			<text>{{item.nickname}}</text>
+			<view class="avatar">
+				<image :src="item.avatar"></image>
+			</view>
+			<text>{{item._id}}</text>
 			<view>{{item.content}}</view>
+			<view v-for="i in item.imgurl">
+				<image :src="i"></image>
+			</view>
 		</view>
+		<uni-pagination :current="anli.current_page" :total="anli.total_count" @change="pageChange"  title="标题文字" />
 	</view>
 </template>
 
@@ -17,15 +24,25 @@
 	} from "@/api/apis.js"
 	const anli = ref([]);
 
-	const getAnli = async () => {
-		let res = await apiAnli({
-		});
+	const getAnli = async (data) => {
+		let res = await apiAnli(data);
 		console.log(res);
 		anli.value = res.data;
 	};
 	getAnli()
+	
+	const pageChange = ({type,current})=>{
+		console.log(type,current)
+		getAnli({page:current})
+	}
 </script>
 
 <style lang="scss" scoped>
+	.avatar {
+		image {
+			width: 50rpx;
+			height: 50rpx;
+		}
 
+	}
 </style>
